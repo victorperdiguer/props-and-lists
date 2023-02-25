e0246b178c27652c0110bc1c9afd9d3bd8604740
Victor Perdiguer <victor.perdiguer@gmail.com>
2023-02-25 14:08:55 +0100

    done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import animalData from './data.json';
 import Card from './components/Card';
 
+
 function App() {
+  const [animals, setAnimals] = useState(animalData);
+
+  const handleSortByLife = () => {
+    const sortedAnimals = [...animalData].sort((a,b) => a.lifeExpectancy - b.lifeExpectancy);
+    setAnimals(sortedAnimals);
+  }
+
+  const handleDogsOnly = () => {
+    const dogsOnly = animalData.filter(animal => animal.type === 'dog');
+    setAnimals(dogsOnly);
+  }
 
   return (
     <div className="App">
       <h1>Adopt me plz 🐾</h1>
-      {/* Some code goes here */}
+      <button onClick={handleSortByLife}>Sort by life expectancy</button>
+      <button onClick={handleDogsOnly}>Show dogs only</button>
+      {animals.map(animal => {
+        return <Card key={animal._id} animal={animal} />
+      })}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Card(props) {
   const { animal: { name, lifeExpectancy, description, needsMedicine, image } } = props;
@@ -6,9 +6,10 @@ export default function Card(props) {
   return (
     <div className="card">
       <h3>{name}</h3>
-      <img src={image} alt={name} width="100%"/>
+      {image ? <img src={image} alt={name} width="100%" /> : <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Dog_silhouette.svg/2067px-Dog_silhouette.svg.png" alt={name} width="100%"/>}
       <p>{description}</p>
       <p>This animal will live {lifeExpectancy} years</p>
+      {needsMedicine && <p className='red-color'>This animal needs medicine</p>}
     </div>
   )
 }
